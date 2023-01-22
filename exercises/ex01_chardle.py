18866006b66b81a35882b49ae2de45aadb477c3f
Josh Braunstein <joshbb@ad.unc.edu>
2023-01-22 18:32:40 -0500

    chardle

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -17,19 +17,20 @@ else:
 if(character not in word):
     print("No instances of " + character + " in " + word)
 
-x: int = 1
+x = 1
 
 if(character == word[0]):
     print(character + " found at index 0")
-    print(x, "instance of " + character + " in " + word)
-
+    print(len(character), " instance ")
 if(character == word[1]):
     print(character + " found at index 1")
-    print(x, "instances of " + character + " in " + word)
-        
+    print(len(character + x))
 if(character == word[2]):
-    print(word[2] + " found at index 2")
+    print(character + " found at index 2")
 if(character == word[3]):
-    print(word[3] + " found at index 3")
+    print(character + " found at index 3")
 if(character == word[4]):
-    print(word[4] + " found at index 4")
+    print(character + " found at index 4")
+
+
+
