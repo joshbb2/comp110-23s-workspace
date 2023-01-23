927c3ff895644e6c3a0b72c414f6d3e326ca643d
Josh Braunstein <joshbb@ad.unc.edu>
2023-01-23 15:27:44 -0500

    instances

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -13,19 +13,20 @@ if((len(character)) != 1):
     print("Error: Character must be a single character.")
 else:
     print("Searching for " + character + " in " + word)
-    if(character not in word):
+if(character not in word):
         print("No instances of " + character + " in " + word)
+
+x = 1
 
 if(word[0] == character):
     print(character + " found at index 0")
-    print("1 instance of " + character + " found in " + word)
+    print(x + " instance of " + character + " found in " + word)
 if(word[1] == character):
     print(character + " found at index 1")
+    print(x + 1, " instances of " + character + " in " + word)
 if(word[2] == character):
     print(character + " found at index 2")
 if(word[3] == character):
     print(character + " found at index 3")
 if(word[4] == character):
     print(character + " found at index 4")
-
-print(len(word))
