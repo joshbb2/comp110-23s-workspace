1d328f637e3af613baf94435702a3621ed8c7ff0
Josh Braunstein <joshbb@ad.unc.edu>
2023-02-02 13:16:09 -0500

    while loop

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -7,9 +7,12 @@ SECRET: str = "python"
 guess: str = input("What is your 6-letters guess? ")
 
 while len(guess) != 6:
-    (input("That was not 6 letters! Try again: "))
-
-if guess == SECRET:
-    print("Woo! You got it!")
+    input("That was not 6 letters! Try again: ")
+    guess = input("That was not 6 letters! Try again: ")
 else:
-    print("Not quite. Play again soon!")
+    if guess == SECRET:
+        print("Woo! You got it!")
+    else:
+        print("Not quite. Play again soon!")
+
+
