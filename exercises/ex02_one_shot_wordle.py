77e9a569353a87fd444724935f0583d2f855faf4
Josh Braunstein <joshbb@ad.unc.edu>
2023-02-05 14:49:26 -0500

    loops

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -4,7 +4,7 @@ __author__ = "730406136"
 
 SECRET: str = "python"
 
-guess: str = input("What is your 6-letters guess? ")
+guess: str = input("What is your 6-letter guess? ")
 
 while len(guess) != 6:
     input("That was not 6 letters! Try again: ")
@@ -20,12 +20,25 @@ result: str = ""
 in_word: bool = False
 
 i = 0
+
          
 while count < len(SECRET):
         if guess[count] == SECRET[count]:
-                result = result + GREEN_BOX
+                result = GREEN_BOX
                 count = count + 1
+while (in_word == False) and i < len(SECRET):
+        if guess[0] == SECRET[i]:
+                in_word = True
+                result = YELLOW_BOX
+                i = i + 1   
+        else:
+                result = WHITE_BOX
+                i = i + 1
+        if guess[1] == SECRET[i]:
+                in_word = True
+                result = YELLOW_BOX
+                i = i + 1   
         else:
                 result = result + WHITE_BOX
-                count = count + 1  
+                i = i + 1           
 print(result)
