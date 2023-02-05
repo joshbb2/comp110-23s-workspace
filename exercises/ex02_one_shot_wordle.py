55ecb7b2ad80d6bf79f540af5bc3f818dd46cd00
Josh Braunstein <joshbb@ad.unc.edu>
2023-02-05 13:50:01 -0500

    while loops

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -13,7 +13,7 @@ WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 
-counter: int = 0
+count: int = 0
 
 result: str = ""
 
@@ -21,50 +21,11 @@ in_word: bool = False
 
 i = 0
          
-if guess[0] == SECRET[0]:
-        result = GREEN_BOX
-        counter = counter + 1
-else:
-        result = WHITE_BOX
-        counter = counter + 1  
-while i < len(guess):
-        if guess[i] == SECRET[i]:
-                in_word = True
-                
-if SECRET[1] == guess[1]:
-        result = result + GREEN_BOX
-        counter = counter + 1
-else:
-        result = result + WHITE_BOX
-        counter = counter + 1
-if SECRET[2] == guess [2]:
-        result = result + GREEN_BOX
-        counter = counter + 1
-else:
-        result = result + WHITE_BOX
-        counter = counter + 1
-if SECRET[3] == guess[3]:
-        result = result + GREEN_BOX
-        counter = counter + 1
-else:
-        result = result + WHITE_BOX
-        counter = counter + 1
-if SECRET[4] == guess[4]:
-        result = result + GREEN_BOX
-        counter = counter + 1
-else:
-        result = result + WHITE_BOX
-        counter = counter + 1
-if SECRET[5] == guess[5]:
-        result = result + GREEN_BOX
-        counter = counter + 1
-else:
-        result = result + WHITE_BOX
-        counter = counter + 1
-
+while counter < len(SECRET):
+        if guess[count] == SECRET[count]:
+                result = result + GREEN_BOX
+                counter = counter + 1
+        else:
+                result = result + WHITE_BOX
+                counter = counter + 1  
 print(result)
-
-if guess == SECRET:
-        print("Woo! You got it!")
-else:
-        print("Not quite. Play again soon!")
