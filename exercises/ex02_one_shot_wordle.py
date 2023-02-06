750102bb7828a18a1c81b05cf73e8862840874a7
Josh Braunstein <joshbb@ad.unc.edu>
2023-02-06 15:37:09 -0500

    loop

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -7,38 +7,37 @@ SECRET: str = "python"
 guess: str = input("What is your 6-letter guess? ")
 
 while len(guess) != 6:
-    input("That was not 6 letters! Try again: ")
+        input("That was not 6 letters! Try again: ")
 
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 
-count: int = 0
+counter = 0
 
 result: str = ""
-       
-while count < len(SECRET):
-        if guess[count] == SECRET[count]:
-                result = result + GREEN_BOX
-                count = count + 1
-        else:
-                result = result + WHITE_BOX
-                count = count + 1
 
 in_word: bool = False
 
-i: int = 0
-
-while in_word == False and i < len(SECRET):
-       if guess[1] == SECRET[i]:
-              in_word = True
-              result[1] == YELLOW_BOX
-              i = i + 1
-       else:
-              i = i + 1
-              
-              
-
+i: int = 1
        
-       
-       print(result)
+while counter < len(SECRET):
+        if guess[counter] == SECRET[counter]:
+                result = result + GREEN_BOX
+                counter = counter + 1
+        else:
+                while in_word == False and i < len(SECRET):
+                        if guess[0] == SECRET[i]:
+                                in_word = True
+                                result = result + YELLOW_BOX
+                                i = i + 1
+                        else:
+                               result = result + WHITE_BOX
+                               i = i + 1
+print(result)
+if guess == SECRET:
+       print("Woo! You got it! ")
+else:
+       print("Not quite. Play again soon! ")
+
+#check if alternate index of secret word is equal to current index of guessed word
