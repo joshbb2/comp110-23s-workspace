b0c7e7471a0283f17e63320c3232fbc73673a0ab
Josh Braunstein <joshbb@ad.unc.edu>
2023-02-19 21:10:07 -0500

    function

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -7,16 +7,16 @@ SECRET: str = "codes"
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
-i: int = 0
 emojified: str = ""
 
 def contains_char(word: str, char: str) -> bool:
     "Determines if char is found at any index of word."
     assert len(char) == 1
+    i: int = 0
     while i < len(word):
         if word[i] == char:
             return True
         else:
             return False
-    i = i + 1
+        i = i + 1
 
