4f8d8a479f10ef382d1cd8d7d0dad7d04ece3656
Josh Braunstein <joshbb@ad.unc.edu>
2023-02-19 22:14:06 -0500

    function defined and calls correctly :)

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -9,14 +9,17 @@ GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 emojified: str = ""
 
+i = 0
+
 def contains_char(word: str, char: str) -> bool:
     "Determines if char is found at any index of word."
     assert len(char) == 1
-    i: int = 0
+    i = 0
     while i < len(word):
         if word[i] == char:
             return True
-        else:
-            return False
-        i = i + 1
+        else: 
+            i = i + 1
+    else:
+        return False
 
