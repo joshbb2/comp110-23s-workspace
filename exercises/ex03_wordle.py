cab0bf67d50902d342b112b554b45a883f1c9227
Josh Braunstein <joshbb@ad.unc.edu>
2023-02-21 23:11:13 -0500

    Part 2 (emojified) COMPLETE ✅

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -8,34 +8,33 @@ WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 
-i = 0
-j = 0
-c = 0
+i: int = 0
+c: int = 0
 
 def contains_char(word: str, char: str) -> bool:
     "Determines if char is found at any index of word."
     assert len(char) == 1
     i = 0
+    in_word: bool = False
     while i < len(word):
         if word[i] == char:
             return True
         else:
-            i = i + 1
-    else:
-        return False
+            i += 1
+    return False
 
 def emojified(guess: str, secret: str) -> str:
     "Determines if any index of guess is equal to any index of secret."
     assert len(guess) == len(secret)
-    j = 0
+    c = 0
     result: str = ""
-    while j < len(secret):
-        if contains_char(secret, guess[j]) == False:
-            result = result + WHITE_BOX
+    while c < len(secret):
+        if guess[c] == secret[c]:
+            result += GREEN_BOX
+        elif contains_char(secret, guess[c]) == True:
+            result += YELLOW_BOX
         else:
-            result = result + YELLOW_BOX
-        j = j + 1
-
+            result += WHITE_BOX
+        c += 1
     return (result)
-    
 
