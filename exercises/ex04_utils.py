0ac0ef5ea0720af0b02ccde99d434b9b5b623038
Josh Braunstein <joshbb@ad.unc.edu>
2023-02-25 18:38:37 -0500

    all complete

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -6,9 +6,14 @@ list_all: list[int] = list()
 
 def all(list_all, integer: int) -> bool:
     i = 0
+    if len(list_all) == 0:
+            return False
     while i < len(list_all) - 1:
         if list_all[i] == integer:
             i += 1
         else:
             return False
     return True
+
+
+    
