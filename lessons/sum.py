9144126a1c471c54f96130ceb8019b15a604569e
Josh Braunstein <joshbb@ad.unc.edu>
2023-03-03 11:33:46 -0500

    CL02

--- lessons/sum.py
+++ lessons/sum.py
@@ -1,14 +1,5 @@
 """Example function for unit tests"""
 
-# def sum(xs: list[float]) -> float:
-#     """return sum of all elements in xs"""
-#     sum_total: float = 0.0
-#     idx: int = 0
-#     while idx < len(xs):
-#         sum_total += xs[idx]
-#         idx += 1
-#     return sum_total
-
 def sum(xs: list[float]) -> float:
     """return sum of all elements in xs"""
     sum_total: float = 0.0
