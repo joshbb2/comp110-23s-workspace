f15f6fe414c02315b8d37a8fe115af2fbeb8d1b0
Josh Braunstein <joshbb@ad.unc.edu>
2023-03-20 22:32:34 -0400

    progress

--- exercises/cyoa.py
+++ exercises/cyoa.py
@@ -11,74 +11,63 @@ def main() -> None:
     global points
     greet()
     menu: str = input("Enter 'A' for the number guessing game. Enter 'B' to exit your adventure. Enter 'C' to pause your adventure.: ")
-    input_menu(menu)
+    invalid_input: bool = False
+    if menu == 'A':
+        random_num()
+    if menu == 'B':
+        exit_game()
+    if menu == 'C':
+        i: int = point_count(points)
+        print(f"You have {i} points!")
+        resume: str = input("Type 'Resume' to unpause your adventure: ")
+        if resume == 'Resume':
+            random_num()
+    else:
+        main()
     
 def greet() -> None:
     global player
     player = input("Welcome to your adventure! What is your name? ")
 
-def input_menu(selection: str) -> None:
-    invalid_input: bool = False
-    if selection == "A":
-        from random import randint
-        answer: int = randint(1,10)
-        print(answer)
-        print(f"=== Turn 1/10 ===")
-        user = input("Guess a number between 1 and 10: ")
-        while user != int in range(1,10):
-            user = input("Guess a number between 1 and 10: ")
-        random_num(user, answer)
-    if selection == "B":
-        exit_game()
-    if selection == "C":
-        i: int = point_count(points)
-        print(f"You have {i} points!")
-        resume: str = input("Type Resume to unpause your adventure: ")
-        if resume == "Resume":
-            input_menu("A")
-    else:
-        invalid_input = True
-        while invalid_input == True:
-            selection = input("Enter A for the number guessing game. Enter B to exit game. Enter C to pause game.: ")
-
-
-def random_num(guess: int, number: int) -> None:
+def random_num() -> None:
     global points
-    turn: int = 2
+    turn: int = 1
     win: bool = False
-    while turn <= 10:
-        print(f"=== Turn {turn}/10 ===")
+    print(f"=== Turn 1/10 ===")
+    from random import randint
+    number = randint(1,10)
+    print(number)
+    guess: int in range(1,10) = input("Guess a number between 1 and 10: ")
+    while turn < 10:
         if guess == number:
             win = True
         else:
-            turn += 1
+            print(f"=== Turn {turn + 1}/10 ===")
             guess = input("Try again! Guess a number between 1 and 10: ")
-    if not win:
-        print(f"{GREEN_BOX} Great job! You got it in {turn - 1} turns! {GREEN_BOX}")
-    else:
+            turn += 1
+    if win == True:
+        print(f"{GREEN_BOX} Great job! You got it in {turn} turns! {GREEN_BOX}")
+    if win == False:
         print(f"{WHITE_BOX} X/10 - Sorry, try again soon! {WHITE_BOX}")
-    exit_game()
-
+        
 def exit_game() -> None:
     global points
     print(f"Goodbye, thanks for playing! Your score is {points} points!")
-    again: str = input("Type 'Yes' to play again: ")
+    again: str = input("Type 'Yes' to start a new adventure: ")
     if again == 'Yes':
         main()
     else:
         exit()
 
-  
 def point_count(score: int) -> int:
     score = 0
-    proceed: str = input(f"{player}, the game has been paused. Type Continue to view your score: ")
-    if proceed == "Continue":
+    proceed: str = input(f"{player}, the game has been paused. Type 'Continue' to view your score: ")
+    if proceed == 'Continue':
         score += 1
     else:
-        while proceed != "Continue":
-            proceed = input(f"{player}, type Continue to proceed: ")
+        while proceed != 'Continue':
+            proceed = input(f"{player}, type 'Continue' to proceed: ")
     return score
 
 if __name__ == "__main__":
     main()
-
