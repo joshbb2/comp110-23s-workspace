e0444db4dd890ed9f7fd4eb88f780de5cb84e565
Josh Braunstein <joshbb@ad.unc.edu>
2023-03-23 13:38:01 -0400

    OA Progress

--- exercises/cyoa.py
+++ exercises/cyoa.py
@@ -2,91 +2,105 @@
 
 __author__ = "730406136"
 
-WHITE_BOX: str = "\U00002B1C"
-GREEN_BOX: str = "\U0001F7E9"
-
+player: str = ""
 points: int = 0
-
-from random import randint
-answer: int = randint(1,10)
+turn: int = 1
 
 def greet() -> None:
     global player
     player = input("Welcome to your adventure! What is your name? ")
 
 def input_guess() -> int:
-    global points
-    user_guess: int in range(1,11) = input("Guess a number between 1 and 10: ")
-    points += 1
+    user_guess: int in range(1,11) = int(input("Guess a number between 1 and 10: "))
     return user_guess
 
-def random_num(guess: int, NUMBER: int) -> bool:
-    win: bool = False
-    if guess == NUMBER:
-        win = True
-    return win
-        
+def random_num() -> bool:
+    global turn
+    correct: bool = False
+    print(f"=== Turn {turn}/10 ===")
+    from random import randint
+    answer: int = randint(1,10)
+    guess: int = input_guess()
+    turn += 1
+    while turn < 11 and not correct:
+        print(f"=== Turn {turn}/10 ===")
+        if guess == answer:
+            correct = True
+        else:
+            guess = input_guess()
+            turn += 1
+    return correct
+    
 def exit_game() -> None:
     print(f"Goodbye, thanks for playing! Your score is {points} points!")
-    print("Type 'Yes' to start a new adventure. \nType 'No' to exit.")
+    print("Type 'Yes' to play again. \nType 'No' to exit.")
     again: str = input("Selection: ")
-    if again == 'Yes':
-        main()
-    if again == 'No':
-        return None
-    while again != 'Yes' or 'No':
-        print("Type 'Yes' to start a new adventure. \nType 'No' to exit.")
-        again = input("Selection: ")
+    cont: bool = False
+    while cont == False:
+        if again == "Yes":
+            cont = True
+            main()
+        if again == "No":
+            cont = True
+            exit()
+        else:
+            again = input("Invalid input. Selection: ")
 
 def point_count(score: int) -> int:
+    global points
     proceed: str = input(f"{player}, the game has been paused. Type 'Continue' to calculate your score: ")
-    if proceed == 'Continue':
-        score += 1
+    if proceed == "Continue":
+        points += 1
     else:
-        while proceed != 'Continue':
+        while proceed != "Continue":
             proceed = input(f"{player}, type 'Continue' to calculate your score: ")
-    return score
+    return points
 
 def main() -> None:
+    global turn
     global points
-    turn: int = 1
+    WHITE_BOX: str = "\U00002B1C"
+    GREEN_BOX: str = "\U0001F7E9"
     greet()
+    points += 1
     print("Enter 'A' for the number guessing game. \nEnter 'B' to exit your adventure. \nEnter 'C' to pause your adventure. ")
     menu: str = input("Selection: ")
     points += 1
-    if menu == 'A':
-        correct: bool = False
-        print(f"=== Turn {turn}/10 ===")
-        attempt: int = input_guess()
-        turn += 1
-        while turn < 11 and not correct:
-            print(f"=== Turn {turn}/10 ===")
-            attempt = input_guess()
-            result = random_num(attempt, answer)
-            if result == True:
-                correct = True
-            else:
-                turn += 1
-        if correct:
+    if menu == "A":
+        points += 1
+        winner = random_num()
+        print(winner)
+        if winner:
             print(f"{GREEN_BOX} Great job! You won in {turn} turns! {GREEN_BOX}")
         else:
             print(f"{WHITE_BOX} X/10 - Sorry, try again soon! {WHITE_BOX}")
-    if menu == 'B':
+        points += 1
         exit_game()
-    if menu == 'C':
-        i = point_count(points)
+    if menu == "B":
+        points += 1
+        exit_game()
+    if menu == "C":
+        points += 1
+        points = point_count(points)
         view_score: str = input("Type 'Score' to view your score: ")
-        if view_score == 'Score':
-            i += 1
-            print(f"You have {i} points!")
+        if view_score == "Score":
+            points += 1
+            print(f"You have {points} points!")
         else:
-            while view_score != 'Score':
+            while view_score != "Score":
                 view_score = input("Type 'Score' to view your score: ")
-        resume: str = input("Type 'Resume' to unpause your adventure: ")
-        if resume == 'Resume':
-            menu == input("Enter 'A' for the number guessing game. \nEnter 'B' to exit your adventure. \nEnter 'C' to pause your adventure. \nSelection: ")
-        while resume != 'Resume':
-            resume = input("Type 'Resume' to unpause your adventure: ")
+        resume: str = input("Type 'Resume' to unpause your adventure. \nType 'Exit' to exit game. \nSelection: ")
+        if resume == "Resume":
+            points += 1
+            main()
+        if resume == "Exit":
+            points += 1
+            exit()
+        while resume != "Resume" or "Exit":
+            resume = input("Invalid input. \nType 'Resume' to unpause your adventure. \nType 'Exit' to exit game. \nSelection: ")
+    else:
+        while menu != "A" or "B" or "C":
+            menu = input("Invalid input. \nEnter 'A' for the number guessing game. \nEnter 'B' to exit your adventure. \nEnter 'C' to pause your adventure. \nSelection: ")
 
 if __name__ == "__main__":
     main()
