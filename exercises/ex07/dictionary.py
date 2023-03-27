2849c7c5069883db7dd4e2d951e233aa7cca2781
Josh Braunstein <joshbb@ad.unc.edu>
2023-03-27 16:14:36 -0400

    EX07 COMPLETE :))

--- exercises/ex07/dictionary.py
+++ exercises/ex07/dictionary.py
@@ -2,9 +2,10 @@
 
 __author__ = "730406136"
 
-def invert(dict_1: dict[str,str]) -> dict[str,str]:
+
+def invert(dict_1: dict[str, str]) -> dict[str, str]:
     """Inverts the keys and values of given dictionary."""
-    result: dict[str,str] = {}
+    result: dict[str, str] = {}
     for key in dict_1:
         if dict_1[key] in result:
             raise KeyError("Value is already in the resulting dictionary.")
@@ -12,7 +13,8 @@ def invert(dict_1: dict[str,str]) -> dict[str,str]:
             result[dict_1[key]] = key
     return result
 
-def favorite_color(dict_1: dict[str,str]) -> str:
+
+def favorite_color(dict_1: dict[str, str]) -> str:
     """Returns color that appears most frequently."""
     all_keys: list[str] = []
     for key in dict_1:
@@ -21,8 +23,10 @@ def favorite_color(dict_1: dict[str,str]) -> str:
     most_common: str = mode(all_keys)
     return most_common
 
-def count(list_1: list[str]) -> dict[str,int]:
-    result: dict[str,int] = {}
+
+def count(list_1: list[str]) -> dict[str, int]:
+    """Returns dictionary with keys as unique values from the input list, and values associated with number of appearances."""
+    result: dict[str, int] = {}
     for item in list_1:
         if item in result:
             result[item] += 1
--- exercises/ex07/dictionary_test.py
+++ exercises/ex07/dictionary_test.py
@@ -6,37 +6,50 @@ from exercises.ex07.dictionary import invert
 from exercises.ex07.dictionary import favorite_color
 from exercises.ex07.dictionary import count
 
+
 def test_invert_use_1() -> None:
+    """Tests use case of invert function."""
     assert invert({'hello': 'world', 'ice': 'cream', 'sun': 'screen'}) == {'world': 'hello', 'cream': 'ice', 'screen': 'sun'}
 
+
 def test_invert_use_2() -> None:
-    assert invert({'a': 'b', 'c': 'd', 'e': 'f', 'g': 'h'}) == {'b': 'a', 'd': 'c', 'f': 'e', 'h':'g'}
+    """Tests use case of invert function."""
+    assert invert({'a': 'b', 'c': 'd', 'e': 'f', 'g': 'h'}) == {'b': 'a', 'd': 'c', 'f': 'e', 'h': 'g'}
+
 
 def test_invert_edge() -> None:
+    """Tests edge case of invert function."""
     import pytest
     with pytest.raises(KeyError):
         my_dictionary = {'monday': 'tuesday', 'wednesday': 'thursday', 'friday': 'tuesday'}
         invert(my_dictionary)
 
+
 def test_favorite_color_use_1() -> None:
+    """Tests use case of favorite_color function."""
     assert favorite_color({'josh': 'blue', 'gabriel': 'red', 'betty': 'blue', 'sam': 'green'}) == 'blue'
 
+
 def test_favorite_color_use_2() -> None:
-    assert favorite_color({'hello': 'pink', 'sunny': 'purple', 'sunny': 'pink', 'entire': 'pink', 'goodbye': 'yellow'}) == 'pink'
+    """Tests use case of favorite_color function."""
+    assert favorite_color({'hello': 'pink', 'sunny': 'purple', 'cloudy': 'pink', 'entire': 'pink', 'goodbye': 'yellow'}) == 'pink'
+
 
 def test_favorite_color_edge() -> None:
+    """Tests edge case of favorite_color function."""
     assert favorite_color({'apple': 'green', 'pear': 'yellow', 'peach': 'red', 'kiwi': 'green', 'pineapple': 'yellow'}) == 'green'
 
+
 def test_count_use_1() -> None:
+    """Tests use case of count function."""
     assert count(['josh', 'gabriel', 'betty', 'trevor', 'josh', 'gabriel', 'josh']) == {'josh': 3, 'gabriel': 2, 'betty': 1, 'trevor': 1}
 
+
 def test_count_use_2() -> None:
+    """Tests use case of count function."""
     assert count(['chocolate', 'vanilla', 'strawberry', 'vanilla', 'strawberry', 'mint', 'vanilla']) == {'chocolate': 1, 'vanilla': 3, 'strawberry': 2, 'mint': 1}
 
+
 def test_count_edge() -> None:
+    """Tests edge case of count function."""
     assert count([]) == {}
-
-
-
-
-
