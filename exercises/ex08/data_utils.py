3b5501bc806c0758202e2d637a81cee48ad1a44d
Josh Braunstein <joshbb@ad.unc.edu>
2023-04-03 16:56:13 -0400

    EX08 COMPLETE 😊✅

--- exercises/ex08/data_utils.py
+++ exercises/ex08/data_utils.py
@@ -1,13 +1,13 @@
-"""EX08 - Data Wrangling (Functions)"""
+"""EX08 - Data Wrangling (Functions)."""
 
 from csv import DictReader
 
 __author__ = "730406136"
 
 
-def read_csv_rows(filename: str) -> list[dict[str,str]]:
-    """Read csv file and return as a list of dicts with header keys"""
-    result: list[dict[str,str]] = []
+def read_csv_rows(filename: str) -> list[dict[str, str]]:
+    """Read csv file and return as a list of dicts with header keys."""
+    result: list[dict[str, str]] = []
     file_handle = open(filename, "r", encoding="utf8")
     csv_reader = DictReader(file_handle)
     for row in csv_reader:
@@ -16,18 +16,18 @@ def read_csv_rows(filename: str) -> list[dict[str,str]]:
     return result
 
 
-def column_values(table: list[dict[str,str]], header: str) -> list[str]:
-    """Returns values in a table column under a specific header"""
+def column_values(table: list[dict[str, str]], header: str) -> list[str]:
+    """Returns values in a table column under a specific header."""
     result: list[str] = []
-    #step through table
+    # step through table
     for row in table:
-        #save every value under key "header"
+        # save every value under key "header"
         result.append(row[header])
     return result
 
 
-def columnar(table: list[dict[str,str]]) -> dict[str, list[str]]:
-    """Reformats data so that it's a dictionary with column headers as keys"""
+def columnar(table: list[dict[str, str]]) -> dict[str, list[str]]:
+    """Reformats data so that it's a dictionary with column headers as keys."""
     result: dict[str, list[str]] = {}
     # loop through keys of one row of table
     first_row: dict[str, str] = table[0]
@@ -41,13 +41,15 @@ def head(original_dict: dict[str, list[str]], rows: int) -> dict[str, list[str]]
     """Produces a new column-based table with only the first N rows of data for each column."""
     smaller_data_set: dict[str, list[str]] = {}
     for key in original_dict:
-            sub_list: list[str] = []
-            data_to_get: list[str] = original_dict[key]
-            j: int = 0
-            while j < rows:
-                sub_list.append(data_to_get[j])
-                j += 1
-            smaller_data_set[key] = sub_list
+        sub_list: list[str] = []
+        data_to_get: list[str] = original_dict[key]
+        if rows >= len(data_to_get):
+            rows = len(data_to_get)
+        j: int = 0
+        while j < rows:
+            sub_list.append(data_to_get[j])
+            j += 1
+        smaller_data_set[key] = sub_list
     return smaller_data_set
 
 
@@ -74,6 +76,7 @@ def concat(dict_1: dict[str, list[str]], dict_2: dict[str, list[str]]) -> dict[s
 
 
 def count(list_1: list[str]) -> dict[str, int]:
+    """Given a list of strings, produces a dict where each key is a unique value in the given list and each associated value is the count of the number of times that value appeared in the input list."""
     result: dict[str, int] = {}
     for item in list_1:
         if item in result:
